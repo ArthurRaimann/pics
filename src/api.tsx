37f0b6e641bc4e97d0baf259ae9180515d889bea
ArthurRaimann <araimann25@gmail.com>
2023-02-22 07:07:05 +0100

    bis ink. 5.67

---
+++ src/api.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const searchImages = async (searchTerm: string) => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+    headers: {
+      Authorization: 'Client-ID hWCOo2a3VY2u0O-xawiq52wazFW5slmjgLNS33B62Tw',
+    },
+    params: {
+      query: searchTerm,
+    },
+  });
+  console.log(response.data.results);
+  return response;
+};
+
+export default searchImages;
